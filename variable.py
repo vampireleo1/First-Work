a8ebfe5f6f557e40f2cb83a184d21b74af5d408e
vampireleo1 <thinkingofleo26@gmail.com>
2020-05-01 00:59:28 +0500

    First Commit

---
+++ variable.py
@@ -0,0 +1,11 @@
+f_name = "Shoaib"
+s_name = "Sultan"
+print ("My Name Is" +f_name + s_name)
+age = "22"
+print ("My Age is" + age)
+a = input ("Enter Your Name")
+print (a)
+b = input ("Enter Your Roll No")
+print (b)
+c = input ("Enter Your Class")
+print (c)
---
+++ work.py
@@ -0,0 +1,16 @@
+a = 45
+b = 324
+c = a+b
+print (c)
+d = 45
+r = 34
+t = d*r
+print (t)
+q = 56
+u = 546
+p = q/u
+print (p)
+g = 987
+m = 64
+v = g-m
+print (v)
